5e9723acd79b04d11d617f2a16ff1cb193064269
Guillaume <guiguib@yahoo.fr>
2015-01-13 18:01:06 +0100

    online/offline

--- yaph.client/src/main/java/com/gbourquet/yaph/client/mvp/view/InlineView.java
+++ yaph.client/src/main/java/com/gbourquet/yaph/client/mvp/view/InlineView.java
@@ -5,7 +5,9 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -17,26 +19,39 @@ public class InlineView extends Composite implements InlinePresenter.View {
 	}
 
 	@UiField
-	Label indication;
+	Button button;
+	
+	@UiField
+	HTMLPanel indication;
+	
+	@UiField
+	Label state;
 	
 	public InlineView() {
 		initWidget(uiBinder.createAndBindUi(this));
+		button.setHeight("20px");
+		button.setWidth("100%");
+		state.setHeight("20px");
+		state.setWidth("100%");
 	}
 
 
 	@Override
 	public void setInline(Boolean inline) {
-		if (inline)
-			indication.setText("Inline");
-		else
-			indication.setText("Offline");
+		if (inline){
+			indication.setStyleName("online");
+			state.setText("online");
+		}
+		else {
+			indication.setStyleName("offline");
+			state.setText("offline");
+		}
 	}
 
 
 	@Override
 	public HasClickHandlers getZone() {
-		// TODO Auto-generated method stub
-		return indication;
+		return button;
 	}
 
 	
